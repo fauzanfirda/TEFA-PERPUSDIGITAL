c302f7209066d8b2b72453724b4e8b91889496e2
fauzanfirda <158239926+fauzanfirda@users.noreply.github.com>
2024-05-14 10:40:59 +0700

    Update index.vue

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -39,7 +39,7 @@ const keyword = ref('')
 
 const getBooks = async () => {
     const { data, error } = await supabase.from('buku').select(`*, kategori(*)`)
-    . ilike('judul', `%${keyword.value}`)
+    .ilike('judul', `%${keyword.value}%`)
     if(data) books.value = data
 }
 
